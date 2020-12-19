0c6d9b28a7507ebe9cf496c7e5d6fe5924b95c3c
Alireza <a.helali1995@example.com>
2020-12-19 19:21:00 +0330

    working with signals

--- cars/signals.py
+++ cars/signals.py
@@ -3,9 +3,14 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from cars.models import Car
+from buyers.models import Buyer
 
 
 @receiver(pre_save, sender=Car)
 def pre_save_create_code(sender, instance, **kwargs):
     if instance.code == '':
         instance.code = str(uuid.uuid4()).replace('-', '')[:10]
+
+    obj = Buyer.objects.get(user=instance.buyer.user)
+    obj.from_signal = True
+    obj.save()
--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from order.models import Order
+
 
 # Create your models here.
+
+class Sale(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.order.name
